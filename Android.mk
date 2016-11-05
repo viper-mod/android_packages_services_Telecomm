ca586ab7652b1132bb3e19b98012352867b0b2ad
sdhz150 <yanghui140@gmail.com>
2016-11-05 14:18:22 +0800

    归属地

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
         ims-ext-common \
         org.cyanogenmod.platform.sdk
 
+LOCAL_STATIC_JAVA_LIBRARIES += libSudaSdk
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-proto-files-under, proto)
 LOCAL_SRC_FILES += \
        src/org/codeaurora/btmultisim/IBluetoothDsdaService.aidl
