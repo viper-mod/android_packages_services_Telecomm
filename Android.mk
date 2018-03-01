430923019d90999d80d6e0e7369d2ca9b8508b6d
sdhz150 <yanghui140@gmail.com>
2018-03-01 11:43:24 +0800

    修复编译
    Change-Id: I23a8c530c09e44bfc5cc600de82810a863c92a32

--- Android.mk
+++ Android.mk
@@ -5,11 +5,11 @@ include $(CLEAR_VARS)
 
 LOCAL_JAVA_LIBRARIES := telephony-common
 
-LOCAL_STATIC_JAVA_LIBRARIES += libSudaSdk
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-proto-files-under, proto)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_STATIC_JAVA_LIBRARIES := org.lineageos.platform.sdk
+LOCAL_STATIC_JAVA_LIBRARIES := \
+	org.lineageos.platform.sdk \
+	libSudaSdk
 
 LOCAL_PROTOC_OPTIMIZE_TYPE := nano
 LOCAL_PROTOC_FLAGS := --proto_path=$(LOCAL_PATH)/proto/
